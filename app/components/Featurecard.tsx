251e791c501794b5ee2496f91e0ae5e5bddce556
Narendra dubey <176411268+narendra-create@users.noreply.github.com>
2026-06-10 19:15:32 +0530

    UPDATE: Increasing font size for noghost card

--- app/components/Featurecard.tsx
+++ app/components/Featurecard.tsx
@@ -11,10 +11,22 @@ type usedpropincard = {
 
 export function Featurecard({ item }: usedpropincard) {
   return (
-    <div className="border border-ink-muted/40 p-5 flex flex-col lg:w-86 lg:h-48 gap-2">
-      {item.isghost && <div className="step-n text-2xl lg:text-4xl">0{item.ghostnumber}</div>}
-      <h3 className="text-[14px] lg:text-[17px]">{item.title}</h3>
-      <p className="text-sm text-ink-muted/78 lg:text-[12px]">{item.body}</p>
+    <div
+      className={`border border-ink-muted/40 p-5 ${!item.isghost ? "lg:min-h-[140px] lg:pt-8": "min-h-[180px]"} flex flex-col w-full gap-2`}
+    >
+      {item.isghost && (
+        <div className="step-n text-2xl lg:text-4xl">0{item.ghostnumber}</div>
+      )}
+      <h3
+        className={`text-[14px] lg:text-[17px] ${item.isghost ? "lg:text-[17px]" : "lg:text-[18px]"}`}
+      >
+        {item.title}
+      </h3>
+      <p
+        className={`text-sm text-ink-muted/78 ${item.isghost ? "lg:text-[12px]" : "lg:text-[14px]"}`}
+      >
+        {item.body}
+      </p>
     </div>
   );
 }

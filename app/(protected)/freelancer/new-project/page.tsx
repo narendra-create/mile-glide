30bb81cb3016f096624747b8270c0f491531d285
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-06-27 20:38:53 +0530

    FEAT: New project form page

---
+++ app/(protected)/freelancer/new-project/page.tsx
@@ -0,0 +1,11 @@
+import { NewProjectForm } from "@/app/components/NewProjectForm";
+
+const NewProject = () => {
+  return (
+    <div className="lg:pl-11 lg:pt-14 mx-4">
+      <NewProjectForm />
+    </div>
+  );
+};
+
+export default NewProject;

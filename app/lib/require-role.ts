11f97f458edb25917a9a21236da0ff76d8af64e0
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-06-27 20:42:18 +0530

    FIX: Fixing header issue by passing headers to better auth session

--- app/lib/require-role.ts
+++ app/lib/require-role.ts
@@ -1,14 +1,14 @@
-import { auth } from "@/auth"
+import { auth } from "@/auth";
+import { headers } from "next/headers";
 
 export async function requireRole(...roles: string[]) {
-    const session = await auth.api.getSession();
-    if (!session) return { error: "Unauthorized", status: 401 }
-    let lowercaserole = [];
-    for (let index = 0; index < roles.length; index++) {
-        lowercaserole.push(roles[index].toLowerCase());
-    }
-    if (!lowercaserole.includes(session.user.role.toLowerCase() ?? "")) return { error: "Forbidden", status: 403 }
-    return { session }
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+    if (!session) return { error: "Unauthorized", status: 401 };
+    const lowercaserole = roles.map((r) => r.toLowerCase());
+    if (!lowercaserole.includes(session.user.role.toLowerCase() ?? "")) return { error: "Forbidden", status: 403 };
+    return { session };
 }
 
 //use this like :-

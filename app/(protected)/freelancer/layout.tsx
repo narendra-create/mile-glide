8bcac71a839429c2fbc6ea74594b03070b406fd2
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-06-28 14:43:02 +0530

    UPDATE: Making layout.tsx server component to fetch real data from database

--- app/(protected)/freelancer/layout.tsx
+++ app/(protected)/freelancer/layout.tsx
@@ -1,18 +1,40 @@
-"use client";
 import { FreelancerSidebar } from "@/app/components/freelancer-sidebar";
 import { ReactNode } from "react";
-import { usePathname } from "next/navigation";
+import { auth } from "@/auth";
+import { headers } from "next/headers";
+import { getFreelancerProfile } from "@/app/lib/controllers/profileController";
+import { getInitials, formatCategory } from "@/app/lib/utilitys";
+import { redirect } from "next/navigation";
 
-export default function Freelancerlayout({
+export default async function Freelancerlayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  const pathname = usePathname();
+  const loadprofiledetails = async () => {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+    if (!session?.user) {
+      redirect("/login");
+    }
+    const result = await getFreelancerProfile(session.user.email);
+    if (!result.profile) {
+      redirect("/unauthorized"); // authenticated but no freelancer record
+    }
+
+    return result.profile;
+  };
+  const data = await loadprofiledetails();
 
   return (
     <div className="flex min-h-screen w-full bg-dash-surface flex-col mt-5">
-      <FreelancerSidebar activePath={pathname} />
+      <FreelancerSidebar
+        image={data.image ?? undefined}
+        initials={getInitials(data.name)}
+        name={data.name}
+        skill={formatCategory(data.Freelancer?.category!)}
+      />
 
       {/* Main Content Area (80% width on desktop, 20% left margin) */}
       <main className="flex-1 pb-[80px] lg:ml-[15%] lg:px-3 lg:w-[85%] lg:pb-0">

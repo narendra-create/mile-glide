8a21e030834beecfce3ab4ef5114dc7bcba8b1a1
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-07-03 16:18:23 +0530

    UPDATE: Created types only for ui

--- types/payment.ts
+++ types/payment.ts
@@ -5,3 +5,26 @@ export type UpiPaymentProps = {
   qrCodeUrl?: string;
   onVerify?: (txnId: string) => void;
 };
+
+import { Paymentstatus } from "@/app/generated/prisma/enums";
+
+export interface PaymentHistory {
+  id: string;
+  projectId?: string | null;
+  project?: {
+    title: string;
+  } | null;
+  total_cost: number;
+  paid_amount: number;
+  payment_status: Paymentstatus;
+  due_date: Date;
+  createdAt: Date;
+  completedAt?: Date | null;
+}
+
+export interface ProjectWithPayments {
+  id: string;
+  title: string;
+  createdAt: Date;
+  payments: PaymentHistory[];
+}

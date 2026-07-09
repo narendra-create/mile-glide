c7b8940e99419b64101b58e97082e1cc7686a54d
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-07-09 10:05:32 +0530

    UPDATE: Making validation tight

--- app/lib/validations/PaymentValidation.ts
+++ app/lib/validations/PaymentValidation.ts
@@ -2,9 +2,8 @@ import z from "zod";
 
 export const intiiatePaymentSchema = z.object({
     txn_number: z.string().min(4, "Too Short"),
-    paymentId: z.string(),
-    paid_amount: z.number(),
-    freelancerId: z.string()
+    paymentId: z.string().min(1),
+    paid_amount: z.number().positive().int()
 });
 
 export type intiiatePaymentInput = z.infer<typeof intiiatePaymentSchema>;

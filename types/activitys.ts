1bd11c4e36b33ae105a065a199374797facf1734
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-07-15 15:13:50 +0530

    UPDATE: Types for activity

---
+++ types/activitys.ts
@@ -0,0 +1,21 @@
+import type { ActivityType } from "@/app/generated/prisma";
+
+export type ActivityItem = {
+  id: string;
+  type: ActivityType;
+  message: string;
+  highlightmessage: string;
+  dateTimeofMessage: Date;
+  projectId: string;
+  project: {
+    title: string;
+  };
+  createdAt: Date;
+};
+
+export type ActivityResponse = {
+  success: boolean;
+  activities?: ActivityItem[];
+  error?: string;
+  status?: number;
+};

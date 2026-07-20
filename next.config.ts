129b2cd18d552d88d7b4dbfda3d058312781cdb7
narendra-create <176411268+narendra-create@users.noreply.github.com>
2026-07-20 13:09:37 +0530

    FEAT: Security headers(CSP) for this website

---
+++ next.config.ts
@@ -0,0 +1,62 @@
+import type { NextConfig } from "next";
+
+
+const cspReportOnly = `default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
+  style-src 'self' 'unsafe-inline' https:;
+  img-src 'self' data: blob: https:;
+  font-src 'self' data: https:;
+  connect-src 'self' https: wss:;
+  frame-ancestors 'none';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  upgrade-insecure-requests;
+`.replace(/\s{2,}/g, " ").trim();
+
+const securityHeaders = [
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff"
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin"
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY"
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload"
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on"
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), browsing-topics=()"
+  },
+  {
+    key: "Content-Security-Policy-Report-Only",
+    value: cspReportOnly
+  }
+]
+
+const nextConfig: NextConfig = {
+  async headers() {
+    return [{
+      source: "/(.*)",
+      headers: securityHeaders
+    }]
+  },
+  allowedDevOrigins: [
+    ...(process.env.NEXT_CONFIG_ALLOWEDORIGINS
+      ?.split(",")
+      .map(origin => origin.trim()) ?? [])
+  ],
+};
+
+export default nextConfig;
